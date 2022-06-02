ba88560baa461c1ace4377115dfb4be3cf2c06c6
josharaedwards <josharaedwards@gmail.com>
2022-06-01 20:54:07 -0700

    You can now get access to local multiplayer

--- Rhythm Game 168/Assets/Scripts/UI/MainMenu.cs
+++ Rhythm Game 168/Assets/Scripts/UI/MainMenu.cs
@@ -25,7 +25,7 @@ public class MainMenu : MonoBehaviour
     public void SinglePlayerButton()
     {
         // Single Player
-        GameManager.gameState = GameManager.GameStates.SinglePlayer;
+        GameManager.instance.SetGameState(GameManager.GameStates.SinglePlayer);
 
         LevelLoaderScript.instance.LoadNextScene("SongSelect");
     }
@@ -33,8 +33,7 @@ public class MainMenu : MonoBehaviour
     public void MultiPlayerButton()
     {
         // Multi Player
-        GameManager.gameState = GameManager.GameStates.SinglePlayer;
-        //GameManager.gameState = GameManager.GameStates.Multiplayer;
+        GameManager.instance.SetGameState(GameManager.GameStates.Multiplayer);
 
         LevelLoaderScript.instance.LoadNextSceneFromWin("SongSelect");
     }
